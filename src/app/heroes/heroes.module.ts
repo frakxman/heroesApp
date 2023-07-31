183d34e0ab2d15aab4f3579d04bf67e9745aa7f7
Fabian Rivera <frakxman@hotmail.com>
2023-07-31 16:53:19 -0500

    Routing settings

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -6,6 +6,7 @@ import { HeroePageComponent } from './pages/heroe-page/heroe-page.component';
 import { ListPageComponent } from './pages/list-page/list-page.component';
 import { NewPageComponent } from './pages/new-page/new-page.component';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { SearchPageComponent } from './pages/search-page/search-page.component';
     HeroePageComponent,
     ListPageComponent,
     NewPageComponent,
-    SearchPageComponent
+    SearchPageComponent,
+    LayoutPageComponent
   ],
   imports: [
     CommonModule,
