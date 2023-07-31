902deeadbedfe45414d5f9adbbc0430820fca73f
Fabian Rivera <frakxman@hotmail.com>
2023-07-31 17:54:03 -0500

    auth html and heroes layout html

--- src/app/heroes/pages/layout-page/layout-page.component.ts
+++ src/app/heroes/pages/layout-page/layout-page.component.ts
@@ -8,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class LayoutPageComponent {
 
+  public sidebarItems = [
+    { label: 'List', icon: 'label', url: './list' },
+    { label: 'Add', icon: 'add', url: './new' },
+    { label: 'Search', icon: 'search', url: './search' },
+    { label: 'Logout', icon: 'logout', url: '/auth/login' },
+
+  ]
 }
